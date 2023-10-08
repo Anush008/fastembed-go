7e503d7867b4a915ef88320e69353ff2d0490f34
Anush008 <46051506+Anush008@users.noreply.github.com>
2023-10-08 19:53:27 +0530

    fix: min(MaxInt32, model_max_length)

--- fastembed.go
+++ fastembed.go
@@ -334,9 +334,9 @@ func loadTokenizer(modelPath string, maxLength int) (*tokenizer.Tokenizer, error
 		return nil, err
 	}
 
-	modelMaxLen := int(tokenizerConfig["model_max_length"].(float64))
-	// Handle overflow when coercing to int
-	maxLength = min(maxLength, int(math.Abs(float64(modelMaxLen))))
+	// Handle overflow when coercing to int, major hassle.
+	modelMaxLen := int(math.Min(float64(math.MaxInt32), math.Abs(tokenizerConfig["model_max_length"].(float64))))
+	maxLength = min(maxLength, modelMaxLen)
 
 	tknzer.WithTruncation(&tokenizer.TruncationParams{
 		MaxLength: maxLength,
