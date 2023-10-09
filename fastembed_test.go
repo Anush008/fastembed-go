d1d4daa8348662aa7744ff1ca97b58c30688a7ae
Anush008 <46051506+Anush008@users.noreply.github.com>
2023-10-09 15:14:38 +0530

    test: canonical value testing

--- fastembed_test.go
+++ fastembed_test.go
@@ -1,7 +1,7 @@
 package fastembed
 
 import (
-	"fmt"
+	"math"
 	"testing"
 )
 
@@ -14,13 +14,12 @@ func TestEmbedBGEBaseEN(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
-	input := []string{"Is the world doing okay?"}
+	input := []string{"hello world"}
 	result, err := fe.Embed(input, 1)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	fmt.Printf("result: %v\n", result[0][0:10])
 	if len(result) != len(input) {
 		t.Errorf("Expected result length %v, got %v", len(input), len(result))
 	}
@@ -35,22 +34,17 @@ func TestEmbedAllMiniLML6V2(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
-	input := []string{"Is the world doing okay?"}
+	input := []string{"hello world"}
 	result, err := fe.Embed(input, 1)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	fmt.Printf("result: %v\n", result[0][0:10])
 	if len(result) != len(input) {
 		t.Errorf("Expected result length %v, got %v", len(input), len(result))
 	}
 }
 
-// Breaks on GH Actions
-// --- FAIL: TestEmbedBGESmallEN (2.29s)
-//
-//	fastembed_test.go:63: Expected no error, got The tensor's shape ([1 512]) requires 512 elements, but only 8 were provided
 func TestEmbedBGESmallEN(t *testing.T) {
 	// Test with a single input
 	fe, err := NewFlagEmbedding(&InitOptions{
@@ -60,13 +54,12 @@ func TestEmbedBGESmallEN(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
-	input := []string{"Is the world doing okay?"}
+	input := []string{"hello world"}
 	result, err := fe.Embed(input, 1)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	fmt.Printf("result: %v\n", result[0][0:10])
 	if len(result) != len(input) {
 		t.Errorf("Expected result length %v, got %v", len(input), len(result))
 	}
@@ -85,7 +78,7 @@ func TestEmbedBGESmallEN(t *testing.T) {
 // 	if err != nil {
 // 		t.Fatalf("Expected no error, got %v", err)
 // 	}
-// 	input := []string{"Is the world doing okay?"}
+// 	input := []string{"hello world"}
 // 	result, err := fe.Embed(input, 1)
 // 	if err != nil {
 // 		t.Fatalf("Expected no error, got %v", err)
@@ -95,3 +88,38 @@ func TestEmbedBGESmallEN(t *testing.T) {
 // 		t.Errorf("Expected result length %v, got %v", len(input), len(result))
 // 	}
 // }
+
+func TestCanonicalValues(T *testing.T) {
+	canonicalValues := map[EmbeddingModel]([]float32){
+		AllMiniLML6V2: []float32{0.02591, 0.00573, 0.01147, 0.03796, -0.02328, -0.05493, 0.014040, -0.01079, -0.02440, -0.01822},
+		BGESmallEN:    []float32{-0.02313, -0.02552, 0.017357, -0.06393, -0.00061, 0.02212, -0.01472, 0.03925, 0.03444, 0.00459},
+		BGEBaseEN:     []float32{0.01140, 0.03722, 0.02941, 0.01230, 0.03451, 0.00876, 0.02356, 0.05414, -0.02945, -0.05472},
+	}
+
+	for model, expected := range canonicalValues {
+		fe, err := NewFlagEmbedding(&InitOptions{
+			Model: model,
+		})
+		defer fe.Destroy()
+		if err != nil {
+			T.Fatalf("Expected no error, got %v", err)
+		}
+		input := []string{"hello world"}
+		result, err := fe.Embed(input, 1)
+		if err != nil {
+			T.Fatalf("Expected no error, got %v", err)
+		}
+
+		if len(result) != len(input) {
+			T.Errorf("Expected result length %v, got %v", len(input), len(result))
+		}
+
+		epsilon := float64(1e-5)
+		for i, v := range expected {
+			if math.Abs(float64(result[0][i]-v)) > float64(epsilon) {
+				T.Errorf("Element %d mismatch: expected %.6f, got %.6f", i, v, result[0][i])
+			}
+		}
+	}
+
+}
